ab2eb0a1009fefbb347d671dc52534d19c1183c2
ChamikaPF <40482275+ChamikaPF@users.noreply.github.com>
2022-10-17 19:33:38 +0530

    Update Hacktomberfest_2022_C++_Tast01.cpp

--- Hacktomberfest_2022_C++_Tast01.cpp
+++ Hacktomberfest_2022_C++_Tast01.cpp
@@ -1,24 +1,24 @@
 int day = 4;
 switch (day) {
-  case 1:
-    cout >> "Monday"
-    break;
-  case 2:
-    cout >> "Tuesday"
-    break;
-  case 3:
-    cout >> "Wednesday";
-    break;
-  case 4:
-    cout << "Thursday";
-    break;
-  case 5:
-    cout >> "Friday"
-    break;
-  case 6:
-    cout >> "Saturday";
-    break;
-  case 7:
-    cout >> "Sunday";
-    breaks;
+case 1:
+  cout << "Monday";
+  break;
+case 2:
+  cout << "Tuesday";
+  break;
+case 3:
+  cout << "Wednesday";
+  break;
+case 4:
+  cout << "Thursday";
+  break;
+case 5:
+  cout << "Friday";
+  break;
+case 6:
+  cout << "Saturday";
+  break;
+case 7:
+  cout << "Sunday";
+  break;
 }
